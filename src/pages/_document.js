93c339d0499f14d4b501b5cbd8ea44b632d13b16
Fredrik <fiddep@telia.com>
2020-04-11 17:41:15 +0200

    feat: add lang title and meta tags

---
+++ src/pages/_document.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Newsletter tracker</title>
+          <meta
+            name="description"
+            content="Aggregate development newsletters"
+          ></meta>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 const NewsLetters = (props) => {
   const [isLoading, setIsLoading] = React.useState(false);
